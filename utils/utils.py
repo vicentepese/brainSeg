2ed109032f084548e124331680e668da3064fb06
vicentepese <sempereperisvicente@gmail.com>
2021-11-04 16:35:53 +0100

    show imgs

---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional as F
+import numpy as np 
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
